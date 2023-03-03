cd03491e19d37f98495c76ad34d1b2d3d68c3bc3
Anastasiia Perova <perovaa.24@gmail.com>
2023-03-03 13:20:02 +0400

    finish task 3.5 - add post (create new person) endpoint

--- index.js
+++ index.js
@@ -5,28 +5,30 @@ let data = [
   {
     id: 1,
     name: "Arto Hellas",
-    number: "040-123456",
+    phone: "040-123456",
   },
   {
     id: 2,
     name: "Ada Lovelace",
-    number: "39-44-5323523",
+    phone: "39-44-5323523",
   },
   {
     id: 3,
     name: "Dan Abramov",
-    number: "12-43-234345",
+    phone: "12-43-234345",
   },
   {
     id: 4,
     name: "Mary Poppendieck",
-    number: "39-23-6423122",
+    phone: "39-23-6423122",
   },
 ];
 
 const personsNumber = data.length;
 
 const app = express();
+
+app.use(express.json());
 
 app.get("/api/persons", (_, response) => {
   response.json(data);
@@ -38,6 +40,22 @@ app.get("/api/persons/:id", (request, response) => {
   if (person) response.json(person);
   response.status(404).end()
 });
+
+app.post("/api/persons", (request, response) => {
+  
+  let id;
+  while (!id) {
+    const tmpId = Math.floor(Math.random() * 100) + 1;
+    console.log(tmpId)
+    if (!data.find((person) => person.id === tmpId)) id = tmpId;
+  }
+  const person = {
+    id,
+    ...request.body
+  }
+  data = [...data, person];
+  response.json(person);
+})
 
 app.delete("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
