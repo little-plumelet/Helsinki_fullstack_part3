6ad8e5d6e18d1d9a523ea5439d832aae93036372
Anastasiia Perova <perovaa.24@gmail.com>
2023-03-03 18:54:35 +0400

    change port

--- index.js
+++ index.js
@@ -104,5 +104,5 @@ app.get("/info", (_, response) => {
   `);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
