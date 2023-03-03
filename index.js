ac76f69fdec1953184bd5d5828c110c16b54de14
Anastasiia Perova <perovaa.24@gmail.com>
2023-03-03 11:44:05 +0400

    finish tassk 3.2 - add endpoint for /info

--- index.js
+++ index.js
@@ -1,33 +1,47 @@
-const express = require('express');
+const express = require("express");
 
 const data = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
   },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
   },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
   },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+const personsNumber = data.length;
 
 const app = express();
 
-app.get('/api/persons', (request, response) => {
-  response.json(data)
-})
+app.get("/api/persons", (request, response) => {
+  response.json(data);
+});
+
+app.get("/info", (request, response) => {
+  const date = new Date().toUTCString();
+  response.send(`
+      <div>
+        Phonebook has info for ${personsNumber} people
+      </div>
+      <div>
+        ${date}
+      </div>
+  `);
+});
 
 const PORT = 3001;
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
