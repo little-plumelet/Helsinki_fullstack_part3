c6f7631bdddc97aa0f0c2a1e4b2c27871ece737b
Anastasiia Perova <perovaa.24@gmail.com>
2023-03-07 20:09:11 +0400

    finish task 3.20 - add validation for phone number

--- models/person.js
+++ models/person.js
@@ -21,7 +21,14 @@ const personSchema = new mongoose.Schema({
   },
   phone: {
     type: String,
-    required: true
+    minLength: 8,
+    required: true,
+    validate: {
+      validator: function(v) {
+        return /\d{2,3}-\d*/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
   }
 });
 
